02a35d0f8518246017d020802ef91020cc7b24b7
Tom Carson <tom.carson1@education.nsw.gov.au>
2025-05-06 13:19:13 +1000

    fixed percentage

--- code/samrad.py
+++ code/samrad.py
@@ -112,7 +112,7 @@ def show_welcome_screen():
         title = font_small.render("Game made by Aaron, Tom, Harry, Jack, Samrad", True, BLACK)
         screen.blit(title, title.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 25)))
 
-        title = font_small.render("Split was 26.5% Aaron, 26.5% Tom, 17.5% Jack, 16% Harry, 13.5% Samrad", True, BLACK)
+        title = font_small.render("Split was 26.5% Aaron, 26.5% Tom, 18.5% Jack, 16% Harry, 12.5% Samrad", True, BLACK)
         screen.blit(title, title.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 10)))
 
         continue_button_rect = pygame.Rect(WIDTH // 2 - button_width // 2, HEIGHT // 2 + 20, button_width, button_height)
